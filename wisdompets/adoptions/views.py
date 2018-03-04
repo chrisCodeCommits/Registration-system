58a6ec2a7708770545141c45bc42624e5642ce6f
Chris <35409541+kristoDev@users.noreply.github.com>
2018-03-04 03:58:12 +0200

    update
    adoption/html

--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404
 
-# Create your views here.
+from .models import Pet
+
+def home(request):
+    pets = Pet.objects.all()
+    return render(request,'home.html', {'pets': pets})
+
+def pet_detial(request, id):
+    try:
+        pet = Pet.objects.get(id=id)
+    except DoesNotExist:
+        raise Http404('pet not found')
+    return render('pet_detail.html', {'pet': pet})
